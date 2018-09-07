10d6607fcb582bc70dd6a41d2b5f2d30ce915dc2
V0idWa1k3r <v0idwa1k39@gmail.com>
2018-09-07 22:36:21 +0300

    Added multi storage support. Closes #1.

--- src/main/java/v0id/vsb/item/upgrade/UpgradeEnderStorage.java
+++ src/main/java/v0id/vsb/item/upgrade/UpgradeEnderStorage.java
@@ -4,10 +4,14 @@ import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.*;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Loader;
@@ -37,9 +41,11 @@ public class UpgradeEnderStorage extends UpgradeFiltered
 {
     private static final Lazy<Class<?>> classEnderStorageManager = new Lazy<>(() -> VSBUtils.getOptionalClass("codechicken.enderstorage.manager.EnderStorageManager", () -> Loader.isModLoaded("enderstorage")));
     private static final Lazy<Class<?>> classFrequency = new Lazy<>(() -> VSBUtils.getOptionalClass("codechicken.enderstorage.api.Frequency", () -> Loader.isModLoaded("enderstorage")));
+    private static final Lazy<Class<?>> classChestHelper = new Lazy<>(() -> VSBUtils.getOptionalClass("shetiphian.multistorage.common.enderlink.ChestHelper", () -> Loader.isModLoaded("multistorage")));
     private static final Lazy<Method> frequency_fromString = new Lazy<>(() -> VSBUtils.getMethodSafe(classFrequency.get(), new Class[]{ String.class, String.class, String.class }, "fromString"));
     private static final Lazy<Method> enderStorageManager_instance = new Lazy<>(() -> VSBUtils.getMethodSafe(classEnderStorageManager.get(), new Class[]{ boolean.class }, "instance"));
     private static final Lazy<Method> enderStorageManager_getStorage = new Lazy<>(() -> VSBUtils.getMethodSafe(classEnderStorageManager.get(), new Class[]{ classFrequency.get(), String.class }, "getStorage"));
+    private static final Lazy<Method> chestHelper_getChest = new Lazy<>(() -> VSBUtils.getMethodSafe(classChestHelper.get(), new Class[]{ World.class, String.class, String.class }, "getChest"));
 
     public UpgradeEnderStorage()
     {
@@ -164,6 +170,113 @@ public class UpgradeEnderStorage extends UpgradeFiltered
         return null;
     }
 
+    private String dyeToCode(ItemStack dye)
+    {
+        if (!dye.isEmpty())
+        {
+            for (String s : VSBUtils.getOreNames(dye))
+            {
+                String code = this.oreDictToCode(s);
+                if (code != null)
+                {
+                    return code;
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private String oreDictToCode(String oreDictName)
+    {
+        if (!oreDictName.startsWith("dye"))
+        {
+            return null;
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeWhite"))
+        {
+            return "0";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeOrange"))
+        {
+            return "1";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeMagenta"))
+        {
+            return "2";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeLightBlue"))
+        {
+            return "3";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeYellow"))
+        {
+            return "4";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeLime"))
+        {
+            return "5";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyePink"))
+        {
+            return "6";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeGray"))
+        {
+            return "7";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeLightGray"))
+        {
+            return "8";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeCyan"))
+        {
+            return "9";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyePurple"))
+        {
+            return "A";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeBlue"))
+        {
+            return "B";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeBrown"))
+        {
+            return "C";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeGreen"))
+        {
+            return "D";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeRed"))
+        {
+            return "E";
+        }
+
+        if (oreDictName.equalsIgnoreCase("dyeBlack"))
+        {
+            return "F";
+        }
+
+        return null;
+    }
+
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn)
     {
@@ -205,15 +318,40 @@ public class UpgradeEnderStorage extends UpgradeFiltered
 
         return null;
     }
+    @Override
+    public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
+    {
+        if (playerIn.isSneaking() && !worldIn.isRemote)
+        {
+            ItemStack is = playerIn.getHeldItem(handIn);
+            if (!is.hasTagCompound())
+            {
+                is.setTagCompound(new NBTTagCompound());
+            }
+
+            is.getTagCompound().setBoolean("multistorage", !is.getTagCompound().getBoolean("multistorage"));
+            worldIn.playSound(null, playerIn.posX, playerIn.posY, playerIn.posZ, SoundEvents.ENTITY_EXPERIENCE_ORB_PICKUP, SoundCategory.PLAYERS, 1, 1);
+            return new ActionResult<>(EnumActionResult.SUCCESS, is);
+        }
+
+        return super.onItemRightClick(worldIn, playerIn, handIn);
+    }
+
+    @Override
+    public EnumActionResult onItemUse(EntityPlayer player, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
+    {
+        return super.onItemUse(player, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+    }
 
     @Override
     public void onTick(@Nullable IBackpackWrapper container, IBackpackWrapper backpack, IUpgradeWrapper self, Entity ticker)
     {
-        if (!Loader.isModLoaded("enderstorage"))
+        if (!Loader.isModLoaded("enderstorage") && !Loader.isModLoaded("multistorage"))
         {
             return;
         }
 
+        boolean useMultiStorage = Loader.isModLoaded("enderstorage") && Loader.isModLoaded("multistorage") ? self.getSelf().hasTagCompound() && self.getSelf().getTagCompound().getBoolean("multistorage") : Loader.isModLoaded("multistorage");
         if (!self.getSelf().hasTagCompound())
         {
             self.getSelf().setTagCompound(new NBTTagCompound());
@@ -235,23 +373,47 @@ public class UpgradeEnderStorage extends UpgradeFiltered
             ItemStack dye2 = inv.getStackInSlot(3);
             if (!dye0.isEmpty() && !dye1.isEmpty() && !dye2.isEmpty())
             {
-                String dyeName0 = this.dyeToColour(dye0);
-                String dyeName1 = this.dyeToColour(dye1);
-                String dyeName2 = this.dyeToColour(dye2);
-                if (dyeName0 != null && dyeName1 != null && dyeName2 != null)
+                if (!useMultiStorage)
                 {
-                    Object frequency = this.tryInvokeMethod(frequency_fromString.get(), null, dyeName0, dyeName1, dyeName2);
-                    Object enderStorageManager = this.tryInvokeMethod(enderStorageManager_instance.get(), null, false);
-                    Object storage = this.tryInvokeMethod(enderStorageManager_getStorage.get(), enderStorageManager, frequency, "item");
-                    if (storage instanceof IInventory)
+                    String dyeName0 = this.dyeToColour(dye0);
+                    String dyeName1 = this.dyeToColour(dye1);
+                    String dyeName2 = this.dyeToColour(dye2);
+                    if (dyeName0 != null && dyeName1 != null && dyeName2 != null)
                     {
-                        IItemHandler itemHandler = new InvWrapper((IInventory) storage);
-                        ItemStack result = ItemHandlerHelper.insertItemStacked(itemHandler, is, true);
-                        if (result != is)
+                        Object frequency = this.tryInvokeMethod(frequency_fromString.get(), null, dyeName0, dyeName1, dyeName2);
+                        Object enderStorageManager = this.tryInvokeMethod(enderStorageManager_instance.get(), null, false);
+                        Object storage = this.tryInvokeMethod(enderStorageManager_getStorage.get(), enderStorageManager, frequency, "item");
+                        if (storage instanceof IInventory)
                         {
-                            ItemHandlerHelper.insertItemStacked(itemHandler, is, false);
-                            int extracted = result.isEmpty() ? is.getCount() : is.getCount() - result.getCount();
-                            backpack.getInventory().extractItem(index, extracted, false);
+                            IItemHandler itemHandler = new InvWrapper((IInventory) storage);
+                            ItemStack result = ItemHandlerHelper.insertItemStacked(itemHandler, is, true);
+                            if (result != is)
+                            {
+                                ItemHandlerHelper.insertItemStacked(itemHandler, is, false);
+                                int extracted = result.isEmpty() ? is.getCount() : is.getCount() - result.getCount();
+                                backpack.getInventory().extractItem(index, extracted, false);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    String dyeCode0 = this.dyeToCode(dye0);
+                    String dyeCode1 = this.dyeToCode(dye1);
+                    String dyeCode2 = this.dyeToCode(dye2);
+                    if (dyeCode0 != null && dyeCode1 != null && dyeCode2 != null)
+                    {
+                        Object chest = this.tryInvokeMethod(chestHelper_getChest.get(), null, ticker.world, "all", dyeCode0 + dyeCode1 + dyeCode2);
+                        if (chest instanceof IInventory)
+                        {
+                            IItemHandler itemHandler = new InvWrapper((IInventory) chest);
+                            ItemStack result = ItemHandlerHelper.insertItemStacked(itemHandler, is, true);
+                            if (result != is)
+                            {
+                                ItemHandlerHelper.insertItemStacked(itemHandler, is, false);
+                                int extracted = result.isEmpty() ? is.getCount() : is.getCount() - result.getCount();
+                                backpack.getInventory().extractItem(index, extracted, false);
+                            }
                         }
                     }
                 }
