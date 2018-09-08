2da0f7bb073b472f8b1a44d25949bbc65ecd7055
V0idWa1k3r <v0idwa1k39@gmail.com>
2018-09-08 13:34:51 +0300

    Backpacks now sync when changing dimensions. Closes #4.

--- src/main/java/v0id/vsb/handler/VSBEventHandler.java
+++ src/main/java/v0id/vsb/handler/VSBEventHandler.java
@@ -8,7 +8,10 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.*;
+import net.minecraft.world.storage.loot.LootEntry;
+import net.minecraft.world.storage.loot.LootEntryTable;
+import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.world.storage.loot.RandomValueRange;
 import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilitySerializable;
@@ -300,6 +303,12 @@ public class VSBEventHandler
             LootPool pool = new LootPool(new LootEntry[] { entry }, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0, 1), "v0idssmartbackpacks_inject_dragon_scales_pool");
             event.getTable().addPool(pool);
         }
+    }
+
+    @SubscribeEvent
+    public static void onDimensionsChanged(net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerChangedDimensionEvent event)
+    {
+        IVSBPlayer.of(event.player).sync();
     }
 
     private static boolean pickupItem(EntityItem item, IBackpackWrapper backpack, EntityPlayer player)
