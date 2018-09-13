c0c8f556cbb6ca86e5ea96374dc02b045c4ce168
V0idWa1k3r <v0idwa1k39@gmail.com>
2018-09-13 21:40:11 +0300

    Magnet upgrade now respects Botania Solegnolia flower. Closes #10.

--- src/main/java/v0id/vsb/item/upgrade/UpgradeMagnet.java
+++ src/main/java/v0id/vsb/item/upgrade/UpgradeMagnet.java
@@ -8,19 +8,27 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.items.CapabilityItemHandler;
 import v0id.api.vsb.capability.IFilter;
 import v0id.api.vsb.data.VSBRegistryNames;
 import v0id.api.vsb.item.IBackpackWrapper;
 import v0id.api.vsb.item.IUpgradeWrapper;
+import v0id.vsb.util.Lazy;
+import v0id.vsb.util.VSBUtils;
 
 import javax.annotation.Nullable;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
 public class UpgradeMagnet extends UpgradeFiltered
 {
+    public static final Lazy<Class<?>> class_SubTileSolegnolia = new Lazy<>(() -> VSBUtils.getOptionalClass("vazkii.botania.common.block.subtile.functional.SubTileSolegnolia", () -> Loader.isModLoaded("botania")));
+    public static final Lazy<Method> subTileSolegnolia_hasSolegnoliaAround = new Lazy<>(() -> VSBUtils.getMethodSafe(class_SubTileSolegnolia.get(), new Class[]{Entity.class}, "hasSolegnoliaAround"));
+
     public UpgradeMagnet()
     {
         super(VSBRegistryNames.itemUpgradeMagnet);
@@ -44,10 +52,32 @@ public class UpgradeMagnet extends UpgradeFiltered
         if (!pulsar.isSneaking() && pulsar instanceof EntityPlayer)
         {
             EntityPlayer player = (EntityPlayer) pulsar;
+            if (this.hasSolegnoliaAround(player))
+            {
+                return;
+            }
+
             IFilter filter = IFilter.of(self.getSelf().getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).getStackInSlot(0));
-            List<EntityItem> items = pulsar.world.getEntitiesWithinAABB(EntityItem.class, new AxisAlignedBB(pulsar.posX - 12, pulsar.posY - 6, pulsar.posZ - 12, pulsar.posX + 12, pulsar.posY + 6, pulsar.posZ + 12), e -> filter == null || filter.accepts(e.getItem()));
+            List<EntityItem> items = pulsar.world.getEntitiesWithinAABB(EntityItem.class, new AxisAlignedBB(pulsar.posX - 12, pulsar.posY - 6, pulsar.posZ - 12, pulsar.posX + 12, pulsar.posY + 6, pulsar.posZ + 12), e -> (filter == null || filter.accepts(e.getItem())) && !this.hasSolegnoliaAround(e));
             items.forEach(e -> e.onCollideWithPlayer(player));
         }
+    }
+
+    private boolean hasSolegnoliaAround(Entity e)
+    {
+        if (Loader.isModLoaded("botania"))
+        {
+            try
+            {
+                return (boolean) subTileSolegnolia_hasSolegnoliaAround.get().invoke(null, e);
+            }
+            catch (Exception ex)
+            {
+                FMLCommonHandler.instance().raiseException(ex, "VSB was unable to hook into Botania", true);
+            }
+        }
+
+        return false;
     }
 
     @Override
